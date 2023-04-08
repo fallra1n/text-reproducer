e3adfacfb36d6dfc4846d8504588788970438c9a
fallra1n <tozhimukhammedov.aa@yandex.ru>
2023-04-08 20:12:08 +0300

    some fix

--- app-gui/app/apl.py
+++ app-gui/app/apl.py
@@ -21,14 +21,14 @@ class App:
     def __init__(self):
         self.win = tk.Tk()
 
-    def initTitle(self):
+    def initTitle(self) -> None:
         self.win.title('Text Reproducer')
 
-    def setGeometry(self):
+    def setGeometry(self) -> None:
         self.win.geometry('1120x700+500+300')
         self.win.maxsize(1120, 700)
 
-    def choosingBtnInit(self):
+    def choosingBtnInit(self) -> None:
         self.choose_btn = tk.Button(
             self.win,
             text="Выбрать файл",
@@ -36,7 +36,7 @@ class App:
         self.choose_btn.config(width=41, height=1)
         self.choose_btn.place(x=400, y=220)
 
-    def playingBtnInit(self):
+    def playingBtnInit(self) -> None:
         self.play_img = Image.open(get_path_to_images() + '/play.jpg')
         resized_image = self.play_img.resize((100, 100))
         self.play_img = ImageTk.PhotoImage(resized_image)
@@ -48,7 +48,7 @@ class App:
             bd=0)
         self.playing_btn.place(x=400, y=250)
 
-    def stoppingBtnInit(self):
+    def stoppingBtnInit(self) -> None:
         self.stop_img = Image.open(get_path_to_images() + '/stop.png')
         resized_image = self.stop_img.resize((100, 100))
         self.stop_img = ImageTk.PhotoImage(resized_image)
@@ -60,7 +60,7 @@ class App:
             bd=0)
         self.stopping_btn.place(x=610, y=250)
 
-    def pauseBtnInit(self):
+    def pauseBtnInit(self) -> None:
         self.pause_img = Image.open(get_path_to_images() + '/pause.png')
         resized_image = self.pause_img.resize((100, 100))
         self.pause_img = ImageTk.PhotoImage(resized_image)
@@ -72,17 +72,17 @@ class App:
             bd=0)
         self.pause_btn.place(x=505, y=250)
 
-    def setBackground(self):
+    def setBackground(self) -> None:
         self.back_img = Image.open(get_path_to_images() + '/back.jpg')
         resized_image = self.back_img.resize((1120, 700))
         self.back_img = ImageTk.PhotoImage(resized_image)
         label = tk.Label(self.win, image=self.back_img)
         label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    def initCV(self):
+    def initCV(self) -> None:
         self.paused_cv = threading.Condition()
 
-    def Run(self):
+    def Run(self) -> None:
         self.setBackground()
         self.initTitle()
         self.setGeometry()
--- app-gui/main.py
+++ app-gui/main.py
@@ -2,9 +2,11 @@ from app.apl import App
 import sys
 import os
 
+# добавим в os.path директорию на один уровень выше текущей,
+# чтобы можно было найти модули из директории back
 sys.path.append(os.path.join(os.getcwd(), '..'))
 
-
+# Запуск приложения
 if __name__ == "__main__":
     app = App()
     app.Run()
